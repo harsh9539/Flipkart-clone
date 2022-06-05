0b38bf32b132c57aaf24000784ce770abe3ee25a
harsh9539 <goyalh862@gmail.com>
2022-06-05 18:08:35 +0530

    1st secton of Home added

--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -1,10 +1,36 @@
+import { Box, styled, Typography } from '@mui/material'
 import React from 'react'
+import { navData } from '../../Constants/data'
 
+//overwriting
+const BoxComp = styled(Box)`
+display:flex;
+margin:55px 130px 0 130px;
+justify-content:space-between;
+`
+const Container = styled(Box)`
+padding:12px 8px;
+text-align:center;
+`
+const Text = styled(Typography)`
+font-size:14px;
+font-weight:600;
+font-family:inherit;
+`
 const Navbar = () => {
     return (
-        <div>
-            heelo from navbar
-        </div>
+        <BoxComp>
+            {
+                navData.map((data)=>{
+                    return(
+                    <Container>
+                        <img src={data.url} alt="nav" style={{width:64}}/>
+                        <Text>{data.text}</Text>
+                    </Container>
+                    )
+                })
+            }
+        </BoxComp>
     )
 }
 
